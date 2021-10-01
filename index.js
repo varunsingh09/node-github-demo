99bf2e1d340874b8ec89877a4f28b3146af6bef3
varunsingh09 <singh.varun1985@gmail.com>
2021-10-01 23:19:23 +0530

    CI/CD implementation

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 const express = require("express");
 
 const app = express();
-
+const PORT = '7000' || process.env.PORT
 // Respond with "Hello World via Github Actions" for requests that hit our root "/"
 app.get("/", function (req, res) {
- return res.send("Hello World via Github Actions");
+  return res.send("Hello World via Github Actions");
 });
 
 // listen to port 7000 by default
-app.listen(process.env.PORT || 7000, () => {
-  console.log("Server is running");
+app.listen(PORT, () => {
+  console.log(`Server is running ${PORT}`);
 });
 
 module.exports = app;
